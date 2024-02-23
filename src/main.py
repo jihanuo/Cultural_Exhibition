bfe4aaf341c013f3b49946b0cb4eda8b9e146f71
吉哈诺 <flaringflame@126.com>
2024-02-23 18:40:09 +0800

    solve conflicts

--- src/main.py
+++ src/main.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template, request, redirect
 from views.auth import auth_blue
 from views.index import index_blue
 from views.admin import admin_blue,dropzone
+from views.about import about_blue
+from views.catalog import catalog_blue , skill_blue
+from views.cultural_intro import cultural_intro_blue
+
 from views.forum import forum_blue
 def create_app():
     app = Flask(__name__, static_folder="static", static_url_path="/static", template_folder="html")
@@ -10,9 +14,10 @@ def create_app():
     dropzone.init_app(app)
     app.register_blueprint(index_blue)
     app.register_blueprint(admin_blue)
+    app.register_blueprint(about_blue)
+    app.register_blueprint(catalog_blue)
+    app.register_blueprint(skill_blue)
+    app.register_blueprint(cultural_intro_blue)
     app.register_blueprint(auth_blue)
     app.register_blueprint(forum_blue)
     return app
-
-
-
