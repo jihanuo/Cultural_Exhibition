24fe14deb48ea842b471ede66cd4b997a8b27658
吉哈诺 <flaringflame@126.com>
2024-02-27 16:44:58 +0800

    晚上项目文件

--- src/main.py
+++ src/main.py
@@ -2,7 +2,6 @@ import settings
 from flask import Flask, render_template, request, redirect
 from views.auth import auth_blue
 from views.index import index_blue
-from views.admin import admin_blue,dropzone
 from views.about import about_blue
 from views.catalog import catalog_blue , skill_blue
 from views.cultural_intro import cultural_intro_blue
@@ -11,9 +10,7 @@ from views.forum import forum_blue
 def create_app():
     app = Flask(__name__, static_folder="static", static_url_path="/static", template_folder="html")
     app.secret_key = 'bello'
-    dropzone.init_app(app)
     app.register_blueprint(index_blue)
-    app.register_blueprint(admin_blue)
     app.register_blueprint(about_blue)
     app.register_blueprint(catalog_blue)
     app.register_blueprint(skill_blue)
