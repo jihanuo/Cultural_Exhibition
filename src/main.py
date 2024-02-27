98fc367455346b0e4c1a18b3498a053b18b2102f
bignosecss <bignosecss@gmail.com>
2024-02-27 19:01:47 +0800

    bignose 进度

--- src/main.py
+++ src/main.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, redirect
 from views.index import index_blue
-from views.admin import admin_blue,dropzone
 from views.about import about_blue
 from views.catalog import catalog_blue , skill_blue, music_blue, food_blue, dance_blue, tale_blue
 from views.cultural_intro import cultural_intro_blue
@@ -8,9 +7,7 @@ from views.news import news_blue
 
 def create_app():
     app = Flask(__name__, static_folder="static", static_url_path="/static", template_folder="html")
-    dropzone.init_app(app)
     app.register_blueprint(index_blue)
-    app.register_blueprint(admin_blue)
     app.register_blueprint(about_blue)
     app.register_blueprint(catalog_blue)
     app.register_blueprint(skill_blue)
